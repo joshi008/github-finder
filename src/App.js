398756bc7541d8c15ca6479a5031ce9c5f1612d2
Hrishabh Joshi <joshi008@users.noreply.github.com>
2020-10-17 01:12:36 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from './components/layouts/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <h1>Hello</h1>
     </div>
   );
