61356b3e8c6513af5e3f83b34d5cf898a16b4589
Hrishabh Joshi <joshi008@users.noreply.github.com>
2020-10-31 02:05:26 +0530

    added some functionalities

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import React, {Component}  from 'react';
+import React, { Component } from 'react';
 import Navbar from './components/layouts/Navbar';
 import './App.css';
-import UserItem from './components/Users/UserItem';
+import Users from './components/Users/Users';
+import Search from './components/Users/Search';
+import axios from 'axios';
 
-class App extends Component{
+class App extends Component {
+
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/users?client_id=$
+    {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ users: res.data, loading: false });
+  }
 
   render() {
     return (
       <div className="App">
         <Navbar></Navbar>
-        <UserItem/>
+        <div className="container">
+          <Search></Search>
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
-  
+
 }
 
 export default App;
