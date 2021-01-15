33847b0e7abf2d09ddc68e679cbc93a3b2c1ff01
Hrishabh Joshi <joshi008@users.noreply.github.com>
2021-01-15 20:20:41 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -22,12 +22,21 @@ class App extends Component {
     this.setState({ users: res.data, loading: false });
   }
 
+  // Search Github Users
+  searchUsers = async (text) => {
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=$
+    {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ users: res.data.items, loading: false });
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar></Navbar>
         <div className="container">
-          <Search></Search>
+          <Search searchUsers={this.searchUsers}></Search>
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
