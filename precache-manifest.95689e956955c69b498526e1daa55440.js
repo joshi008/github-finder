991bc3c127649d09a323f2e70ab634c32fabf812
Hrishabh Joshi <joshi008@users.noreply.github.com>
2021-01-15 20:27:54 +0530

    build static

---
+++ precache-manifest.95689e956955c69b498526e1daa55440.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c170287b053aeb6f0ffb19406aa4677",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bd5f4cf82135133d016",
+    "url": "/static/css/main.2e508846.chunk.css"
+  },
+  {
+    "revision": "5fcfeb723c57bd6add6a",
+    "url": "/static/js/2.2fee9d1c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.2fee9d1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bd5f4cf82135133d016",
+    "url": "/static/js/main.fbc2d3f3.chunk.js"
+  },
+  {
+    "revision": "25d48e391914dc08b45e",
+    "url": "/static/js/runtime-main.d89612ff.js"
+  }
+]);
